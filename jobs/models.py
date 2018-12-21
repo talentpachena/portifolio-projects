8c42580113bed399ebd07a1988de038250889579
talent pachena <talentpachena@talents-MacBook-Air.local>
2018-12-21 13:43:03 +0200

    added MEDIA_ROOT, MEDIA_URL and imported static and settings module

--- jobs/models.py
+++ jobs/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class job(models.Model):
     image = models.ImageField(upload_to='image/')
-    summary = models.CharField(max_lenth=75)
+    summary = models.CharField(max_length=200)
